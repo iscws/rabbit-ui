d10a81420b0a45802b312fba20bc50d3b5b2a957
cws <2354554898@qq.com>
2023-02-05 17:12:58 +0800

    feat: 配置项修改

--- .dumi/tmp/dumi/meta/index.ts
+++ .dumi/tmp/dumi/meta/index.ts
@@ -3,8 +3,9 @@
 // DO NOT CHANGE IT MANUALLY!
 import { demos as dm0, frontmatter as fm0, toc as toc0, texts as txt0 } from 'D:/Desktop/rabbit-ui/docs/index.md?type=meta';
 import { demos as dm1, frontmatter as fm1, toc as toc1, texts as txt1 } from 'D:/Desktop/rabbit-ui/src/alert/index.md?type=meta';
-import { demos as dm2, frontmatter as fm2, toc as toc2, texts as txt2 } from 'D:/Desktop/rabbit-ui/src/modal/index.md?type=meta';
-import { demos as dm3, frontmatter as fm3, toc as toc3, texts as txt3 } from 'D:/Desktop/rabbit-ui/src/space/index.md?type=meta';
+import { demos as dm2, frontmatter as fm2, toc as toc2, texts as txt2 } from 'D:/Desktop/rabbit-ui/src/button/index.md?type=meta';
+import { demos as dm3, frontmatter as fm3, toc as toc3, texts as txt3 } from 'D:/Desktop/rabbit-ui/src/modal/index.md?type=meta';
+import { demos as dm4, frontmatter as fm4, toc as toc4, texts as txt4 } from 'D:/Desktop/rabbit-ui/src/space/index.md?type=meta';
 
 export { components } from './atoms';
 export { tabs } from './tabs';
@@ -22,17 +23,23 @@ export const filesMeta = {
     texts: txt1,
     demos: dm1,
   },
-  'components/modal/index': {
+  'components/button/index': {
     frontmatter: fm2,
     toc: toc2,
     texts: txt2,
     demos: dm2,
   },
-  'components/space/index': {
+  'components/modal/index': {
     frontmatter: fm3,
     toc: toc3,
     texts: txt3,
     demos: dm3,
+  },
+  'components/space/index': {
+    frontmatter: fm4,
+    toc: toc4,
+    texts: txt4,
+    demos: dm4,
   },
 }
 
