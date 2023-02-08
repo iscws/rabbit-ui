e330bdfb83aae0f583f464dfdb5222891d6dd5ce
sinsuke-301 <820681094@qq.com>
2023-02-08 10:49:34 +0800

    feat:修改配置项

--- .umirc.ts
+++ .umirc.ts
@@ -5,12 +5,9 @@ const repo = 'rabbit-ui';
 export default defineConfig({
   title: 'Rabbit UI', // 站点名称
   mode: 'site',
-  devSever: {
-    port: 8080//部署端口
-  },
   outputPath: 'doc-site', // 输出文件夹
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : undefined,
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : undefined,
   exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
 });
