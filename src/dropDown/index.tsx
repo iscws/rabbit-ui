806764e6f35d92032663741beca54715f548a055
sinsuke-301 <820681094@qq.com>
2023-02-08 22:13:24 +0800

    feat:修改dropdown部分配置

--- src/dropDown/index.tsx
+++ src/dropDown/index.tsx
@@ -3,6 +3,7 @@ import PropTypes, { func } from 'prop-types';
 import t from 'prop-types';
 import Trigger from 'rc-trigger';
 import 'rc-trigger/assets/index.css';
+import './style'
 
 
 export interface DropDownProps {
