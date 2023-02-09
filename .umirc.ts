274476f36b4282cf6e6b5ea01f578e2d79424eff
sinsuke-301 <820681094@qq.com>
2023-02-09 10:37:12 +0800

    feat:修改部分配置

--- .umirc.ts
+++ .umirc.ts
@@ -2,20 +2,15 @@ import { defineConfig } from 'dumi';
 
 const repo = 'rabbit-ui';
 
-let base = '/rabbit-ui';
-let publicPath = '/rabbit-ui/';
-
-if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
-  base = undefined;
-  publicPath = undefined;
-}
-
 export default defineConfig({
   title: 'Rabbit UI', // 站点名称
   mode: 'site',
+  devSever: {
+    port: 8080
+  },
   outputPath: 'doc-site', // 输出文件夹
-  base,
-  publicPath,
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : undefined,
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : undefined,
   exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
 });
