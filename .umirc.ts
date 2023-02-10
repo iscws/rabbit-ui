476612312b61184ad7e9060cf6671da6771e627e
sinsuke-301 <820681094@qq.com>
2023-02-10 20:24:44 +0800

    feat:合并dev

--- .umirc.ts
+++ .umirc.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'dumi';
 
-let base = '/rabbit-ui';
-let publicPath = '/rabbit-ui/';
-
-if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
-  base = undefined;
-  publicPath = undefined;
-}
+const repo = 'rabbit-ui';
 
 export default defineConfig({
-  title: 'Rabbit UI',
+  title: 'Rabbit UI', // 站点名称
   mode: 'site',
-  outputPath: 'dist',
-  exportStatic: {},
-  dynamicImport: {},
-  base,
-  publicPath,
+  outputPath: 'doc-site', // 输出文件夹
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : undefined,
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : undefined,
+  exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
+  dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
 });
