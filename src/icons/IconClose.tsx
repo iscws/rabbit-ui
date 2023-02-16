2c75e3be1e3bcd57debded1177a12c99afe6d1af
cws <2354554898@qq.com>
2023-02-16 18:19:13 +0800

    fix: 修复github pages失效

---
+++ src/icons/IconClose.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IconClose() {
+    return (
+        <svg width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 8L40 40" stroke="#333" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><path d="M8 40L40 8" stroke="#333" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /></svg>
+    )
+}
