c4c6d2ef9f36b1b2a3b31cf08c49eb0b079a745c
hgc <2376371452@qq.com>
2023-02-19 11:58:41 +0800

    feat:更新版本

--- .dumi/tmp/dumi/locales/runtime.tsx
+++ .dumi/tmp/dumi/locales/runtime.tsx
@@ -3,8 +3,8 @@
 // DO NOT CHANGE IT MANUALLY!
 import { history } from 'dumi';
 import React, { useState, useLayoutEffect, useCallback, type ReactNode } from 'react';
-import { RawIntlProvider, createIntl, createIntlCache } from 'C:/Users/82068/Desktop/rabbit-UI/rabbit-ui/node_modules/react-intl';
-import { useIsomorphicLayoutEffect } from 'C:/Users/82068/Desktop/rabbit-UI/rabbit-ui/node_modules/dumi/dist/client/theme-api/utils.js'
+import { RawIntlProvider, createIntl, createIntlCache } from 'E:/MyProject_Web/Rabbit组件库开发/rabbit-ui/node_modules/react-intl';
+import { useIsomorphicLayoutEffect } from 'E:/MyProject_Web/Rabbit组件库开发/rabbit-ui/node_modules/dumi/dist/client/theme-api/utils.js'
 import { locales, messages } from './config';
 
 const cache = createIntlCache();
--- .dumi/tmp/umi.ts
+++ .dumi/tmp/umi.ts
@@ -3,7 +3,7 @@
 // DO NOT CHANGE IT MANUALLY!
 import './core/polyfill';
 
-import { renderClient } from 'C:/Users/82068/Desktop/rabbit-UI/rabbit-ui/node_modules/@umijs/renderer-react';
+import { renderClient } from 'E:/MyProject_Web/Rabbit组件库开发/rabbit-ui/node_modules/@umijs/renderer-react';
 import { getRoutes } from './core/route';
 import { createPluginManager } from './core/plugin';
 import { createHistory } from './core/history';
@@ -97,5 +97,5 @@ if (
 render();
 
 window.g_umi = {
-  version: '4.0.52',
+  version: '4.0.46',
 };
