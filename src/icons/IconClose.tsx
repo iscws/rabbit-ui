435177a42a8c62670bcfd44f76a61635d0767ddf
cws <2354554898@qq.com>
2023-02-21 22:45:10 +0800

    fix: 解决合并冲突

--- src/icons/IconClose.tsx
+++ src/icons/IconClose.tsx
@@ -5,11 +5,6 @@ interface iconProps {
   size?: string;
 }
 export default function IconClose<iconProps>({ color = '#333', size = '16' }) {
-<<<<<<< HEAD
-    return (
-        <svg role="img" width={size} height={size} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 8L40 40" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /><path d="M8 40L40 8" stroke={color} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" /></svg>
-    )
-=======
   return (
     <svg
       width={size}
@@ -34,5 +29,4 @@ export default function IconClose<iconProps>({ color = '#333', size = '16' }) {
       />
     </svg>
   );
->>>>>>> 8333b0f6ff5fc4177c3e68b675d6ece013b01fbe
 }
